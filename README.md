ed957713bcc115d0659ec779c94a590076a82ceb
massamba <massamba@expemle.com>
2025-11-19 22:28:51 +0000

    feat: add initial stock management files

---
+++ README.md
@@ -0,0 +1,14 @@
+# 🧮 Application de gestion de stock
+
+Une mini-application Python pour gérer le stock d’une boutique et générer des factures simples.
+
+## 📦 Fonctionnalités
+- Ajouter et afficher les produits
+- Enregistrer une vente
+- Met à jour le stock automatiquement
+- Génère une facture après chaque vente
+- Base de données SQLite intégrée
+
+## 🚀 Lancer le projet
+```bash
+python main.py
--- models/produit.py
+++ models/produit.py
@@ -1,1 +1,24 @@
 from database.db_setup import get_connection
+
+def ajouter_produit(nom,categorie,prix_carton,quantite):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+    INSERT INTO produits (nom,categorie,prix_unitaire,quantite)
+    VALUES(?,?,?,?)               
+    """,(nom,categorie,prix_carton,quantite))
+    conn.commit()
+    conn.close()
+    print(f"Produit '{nom}' ajouté avec succès.")
+
+
+def afficher_produits():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM produits")
+    produits = cursor.fetchall()
+    conn.close()
+
+    print("\n Liste des produits : ")
+    for p in produits :
+        print(f"{p[0]} | {p[1]} | {p[2]} | {p[3]} CFA | Stock: {p[4]}")
