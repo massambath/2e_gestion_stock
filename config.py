cec2e1ed9dc344dc724b1f7469ca1b6140f59370
massamba <massbatht@gmail.com>
2025-12-11 01:46:34 +0000

    Version Supabase prête pour déploiement RESOL

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ from supabase import create_client
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Les variables SUPABASE_URL et SUPABASE_KEY doivent être définies dans l'environnement")
+
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
