7b08b4c0206b3f43db1093b80b550114442b71d0
massamba <massbatht@gmail.com>
2025-12-12 01:48:23 +0000

    Mise à jour contrainte unique reference

--- models/produit.py
+++ models/produit.py
@@ -1,13 +1,17 @@
 from config import supabase
-
+from postgrest.exceptions import APIError
 def ajouter_produit(reference,nom,categorie,prix_carton,quantite):
-    supabase.table("produits").insert({
+    try:
+        supabase.table("produits").insert({
         "reference": reference,
         "nom": nom,
         "categorie": categorie,
         "prix_unitaire": prix_carton,
         "quantite": quantite
-    }).execute()
+        }).execute()
+        return f"✔ Produit {reference} ajouté avec succès."
+    except APIError:
+        return f"❌ Impossible d’ajouter : le produit avec la référence {reference} existe déjà."
 
 
 def afficher_produits(return_df=False):
